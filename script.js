fb7d9637a27a7783d3521ba4ed9976cef1dbcd83
Maisaraharfan <maisaraharfan@gmail.com>
2025-04-19 06:44:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
-const sections = document.querySelectorAll('.section');
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  const mainContent = document.getElementById('main-content');
+  sidebar.classList.toggle('inactive');
+  mainContent.classList.toggle('shifted');
+}
+
 function showSection(id) {
-  sections.forEach(sec => sec.classList.remove('active'));
+  document.querySelectorAll('.content-section').forEach(sec => {
+    sec.classList.remove('active');
+  });
   document.getElementById(id).classList.add('active');
 }
-document.getElementById('toggleSidebar').addEventListener('click', () => {
-  document.getElementById('sidebar').classList.toggle('hidden');
-});
